311330f3f3c418a938cfca9405597d74f88a48d7
Walter Grönholm <waxwax@dhcp-asv-103.helsinki.fi>
2017-01-26 22:38:11 +0200

    HTML tehty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,32 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        //View::make('home.html');
+        View::make('aiheet.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('login.html');
     }
-  }
+
+    public static function kurssi_lista() {
+        View::make('kurssilista_op.html');
+    }
+    
+    public static function kurssi_lista_kv() {
+        View::make('kurssilista_kv.html');
+    }
+
+    public static function kurssi() {
+        View::make('kurssi.html');
+    }
+
+    public static function login() {
+        View::make('login.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,29 @@
 <?php
 
-  $routes->get('/', function() {
-    HelloWorldController::index();
-  });
+$routes->get('/', function() {
+HelloWorldController::index();
+});
 
-  $routes->get('/hiekkalaatikko', function() {
-    HelloWorldController::sandbox();
-  });
+$routes->get('/hiekkalaatikko', function() {
+HelloWorldController::sandbox();
+});
+
+$routes->get('/aiheet', function() {
+HelloWorldController::index();
+});
+
+$routes->get('/kurssit', function() {
+HelloWorldController::kurssi_lista();
+});
+
+$routes->get('/kurssit_kv', function() {
+HelloWorldController::kurssi_lista_kv();
+});
+
+$routes->get('/kurssi/1', function() {
+HelloWorldController::kurssi();
+});
+
+$routes->get('/login', function() {
+HelloWorldController::login();
+});
